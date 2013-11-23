bec7a73e801886c42d0b52e566a146fe13ede529
Ernest W. Durbin III <ewdurbin@gmail.com>
2013-11-23 09:38:15 -0500

    remove custom backup script in favor of pg_basebackup

--- provisioning/salt/roots/salt/postgresql/cluster/config/base-backup.bash
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-psql -c "SELECT pg_start_backup('replbackup');"
-tar cfP /var/lib/pgsql/9.3/backups/master_base_backup-$(date +"%s").tar /var/lib/pgsql/9.3/data \
-  --exclude=/var/lib/pgsql/9.3/data/postgresql.conf \
-  --exclude=/var/lib/pgsql/9.3/data/recovery.conf \
-  --exclude=/var/lib/pgsql/9.3/data/recovery.done
-psql -c "SELECT pg_stop_backup();"
